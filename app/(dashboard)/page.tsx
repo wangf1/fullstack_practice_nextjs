3944931a087cca21cc160c15fa8f0ea744fd96e7
F W <armstrong.wang2000@gmail.com>
2024-06-15 16:15:37 -0600

    Create account sheet, zustand for state management

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,15 +1,23 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function Home() {
+  const { onOpen } = useNewAccount();
+
   const { data, isLoading } = useGetAccounts();
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
   if (!data || data.length === 0) {
-    return <div>No accounts</div>;
+    return (
+      <div>
+        <Button onClick={onOpen}>Add an Account</Button>
+      </div>
+    );
   }
 
   return (
