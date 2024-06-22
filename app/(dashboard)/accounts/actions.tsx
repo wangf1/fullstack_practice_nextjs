d018681a90c986955cdc0fe644c8691e51928e20
Mark Wang <armstrong.wang2000@gmail.com>
2024-06-21 22:55:51 -0600

    Delete in row menu.

--- app/(dashboard)/accounts/actions.tsx
+++ app/(dashboard)/accounts/actions.tsx
@@ -6,17 +6,32 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
 import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
+import { useConfirm } from "@/hooks/use-confirm";
 import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
 
 type Props = { id: string };
 
 export const Actions = ({ id }: Props) => {
   const { onOpen } = useOpenAccount();
+  const deleteMutaiotn = useDeleteAccount(id);
 
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Delete Account",
+    "Are you sure you want to delete this account?"
+  );
+
+  const onDelete = async () => {
+    const ok = await confirm();
+    if (ok) {
+      deleteMutaiotn.mutate();
+    }
+  };
   return (
     <>
+      <ConfirmDialog />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="size-8 p-0">
@@ -27,6 +42,10 @@ export const Actions = ({ id }: Props) => {
           <DropdownMenuItem disabled={false} onClick={() => onOpen(id)}>
             <Edit className="size-4 mr-2" />
             Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem disabled={false} onClick={() => onDelete()}>
+            <Trash className="size-4 mr-2" />
+            Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
