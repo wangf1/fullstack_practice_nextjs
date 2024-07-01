c1ddd58f51f8bf7def262cb42260b7ad9e009dd3
Mark Wang <armstrong.wang2000@gmail.com>
2024-06-30 23:46:30 -0600

    Edit invoice

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -32,3 +32,17 @@ export async function createInvoice(formData: FormData) {
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  await sql`UPDATE invoices
+  SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+  WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
